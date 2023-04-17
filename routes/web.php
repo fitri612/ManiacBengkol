39d0b9d6367409f808c028a688276b4a85770b5a
Aldi <20sa1132@amikompurwokert.ac.id>
2023-04-18 00:40:54 +0700

    login register mentahan include admin role

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::view('/nav', 'partials.navbar');
+
+Route::middleware(['admin'])->group(function () { 
+
+});
