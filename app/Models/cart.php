3350a12aa7a9b726567397412c89408d02a3eae9
Aldi <20sa1132@amikompurwokert.ac.id>
2023-05-17 21:40:45 +0700

    pppp

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cart extends Model
 {
