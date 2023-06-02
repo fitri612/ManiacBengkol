0cf8f4cbe7fc13974be16aab113124399cbea29d
xellyx <79205130+xellyx@users.noreply.github.com>
2023-06-03 04:36:47 +0700

    asdas

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -140,7 +140,7 @@
 
         <nav class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
             <ul
-                class="navigation flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                class="navigation flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 dark:text-white">
                 <li><a href="{{ url('/') }}" class="nav-item" data-nav="beranda">Beranda</a></li>
                 <li><a href="{{ url('/articles') }}" class="nav-item" data-nav="artikel">Artikel</a></li>
                 <li><a href="{{ url('/product-list') }}" class="nav-item" data-nav="daftarproduk">Daftar Produk</a>
