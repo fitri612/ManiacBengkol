f167d9570f4c41b5f1e153b01f4829a73e4c70d9
xellyx <79205130+xellyx@users.noreply.github.com>
2023-06-04 09:02:57 +0700

    asd

--- resources/views/partials/admin/sidebar/index.blade.php
+++ resources/views/partials/admin/sidebar/index.blade.php
@@ -5,10 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/sidebar.css') }}">
 </head>
-<body>
-
+<body> 
 <div class="sidebar print:hidden">
     <!-- Main Sidebar -->
     <div class="main-sidebar">
@@ -18,97 +17,14 @@
             <div class="flex pt-4">
                 <a href="#">
                     <img class="h-11 w-11 transition-transform duration-500 ease-in-out hover:rotate-[360deg]"
-                        src="{{ asset('assets/app-logo.svg') }}" alt="Lineone Logo" />
+                    src="https://gcdnb.pbrd.co/images/4LUQpWQfxfra.png?o=1" alt="maniac Logo" />
                 </a>
             </div>
-            <div class="is-scrollbar-hidden flex grow flex-col space-y-4 overflow-y-auto pt-6">
-                <!-- Dashobards -->
-
-                <div class="sidebar">
-                    <ul>
-                      <li><a href="{{ url('/category') }}" class="sidebar-item flex h-11 w-11 items-center justify-center rounded-lg bg-primary/10 text-primary outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:bg-navy-600 dark:text-accent-light dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90"
-                        x-tooltip.placement.right="'Category'" data-sidebar="home"> 
-                        <svg class="h-7 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <path fill="currentColor" fill-opacity=".3"
-                                d="M5 14.059c0-1.01 0-1.514.222-1.945.221-.43.632-.724 1.453-1.31l4.163-2.974c.56-.4.842-.601 1.162-.601.32 0 .601.2 1.162.601l4.163 2.974c.821.586 1.232.88 1.453 1.31.222.43.222.935.222 1.945V19c0 .943 0 1.414-.293 1.707C18.414 21 17.943 21 17 21H7c-.943 0-1.414 0-1.707-.293C5 20.414 5 19.943 5 19v-4.94Z" />
-                            <path fill="currentColor"
-                                d="M3 12.387c0 .267 0 .4.084.441.084.041.19-.04.4-.204l7.288-5.669c.59-.459.885-.688 1.228-.688.343 0 .638.23 1.228.688l7.288 5.669c.21.163.316.245.4.204.084-.04.084-.174.084-.441v-.409c0-.48 0-.72-.102-.928-.101-.208-.291-.355-.67-.65l-7-5.445c-.59-.459-.885-.688-1.228-.688-.343 0-.638.23-1.228.688l-7 5.445c-.379.295-.569.442-.67.65-.102.208-.102.448-.102.928v.409Z" />
-                            <path fill="currentColor"
-                                d="M11.5 15.5h1A1.5 1.5 0 0 1 14 17v3.5h-4V17a1.5 1.5 0 0 1 1.5-1.5Z" />
-                            <path fill="currentColor"
-                                d="M17.5 5h-1a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5Z" />
-                        </svg></a></li>
-                      <li><a href="{{ url('/product') }}" class="sidebar-item flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
-                        x-tooltip.placement.right="'Product'" data-sidebar="about">
-                        <svg class="h-7 w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M5 8H19V16C19 17.8856 19 18.8284 18.4142 19.4142C17.8284 20 16.8856 20 15 20H9C7.11438 20 6.17157 20 5.58579 19.4142C5 18.8284 5 17.8856 5 16V8Z"
-                                fill="currentColor" fill-opacity="0.3" />
-                            <path
-                                d="M12 8L11.7608 5.84709C11.6123 4.51089 10.4672 3.5 9.12282 3.5V3.5C7.68381 3.5 6.5 4.66655 6.5 6.10555V6.10555C6.5 6.97673 6.93539 7.79026 7.66025 8.2735L9.5 9.5"
-                                stroke="currentColor" stroke-linecap="round" />
-                            <path
-                                d="M12 8L12.2392 5.84709C12.3877 4.51089 13.5328 3.5 14.8772 3.5V3.5C16.3162 3.5 17.5 4.66655 17.5 6.10555V6.10555C17.5 6.97673 17.0646 7.79026 16.3397 8.2735L14.5 9.5"
-                                stroke="currentColor" stroke-linecap="round" />
-                            <rect x="4" y="8" width="16" height="3" rx="1"
-                                fill="currentColor" />
-                            <path d="M12 11V15" stroke="currentColor" stroke-linecap="round" />
-                        </svg>
-                    </a></li>
-                      <li><a href="{{ url('/booking-admin') }}" class="sidebar-item flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
-                        x-tooltip.placement.right="'Booking Admin'" data-sidebar="services">
-                        <svg class="h-7 w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M9.85714 3H4.14286C3.51167 3 3 3.51167 3 4.14286V9.85714C3 10.4883 3.51167 11 4.14286 11H9.85714C10.4883 11 11 10.4883 11 9.85714V4.14286C11 3.51167 10.4883 3 9.85714 3Z"
-                                fill="currentColor" />
-                            <path
-                                d="M9.85714 12.8999H4.14286C3.51167 12.8999 3 13.4116 3 14.0428V19.757C3 20.3882 3.51167 20.8999 4.14286 20.8999H9.85714C10.4883 20.8999 11 20.3882 11 19.757V14.0428C11 13.4116 10.4883 12.8999 9.85714 12.8999Z"
-                                fill="currentColor" fill-opacity="0.3" />
-                            <path
-                                d="M19.757 3H14.0428C13.4116 3 12.8999 3.51167 12.8999 4.14286V9.85714C12.8999 10.4883 13.4116 11 14.0428 11H19.757C20.3882 11 20.8999 10.4883 20.8999 9.85714V4.14286C20.8999 3.51167 20.3882 3 19.757 3Z"
-                                fill="currentColor" fill-opacity="0.3" />
-                            <path
-                                d="M19.757 12.8999H14.0428C13.4116 12.8999 12.8999 13.4116 12.8999 14.0428V19.757C12.8999 20.3882 13.4116 20.8999 14.0428 20.8999H19.757C20.3882 20.8999 20.8999 20.3882 20.8999 19.757V14.0428C20.8999 13.4116 20.3882 12.8999 19.757 12.8999Z"
-                                fill="currentColor" fill-opacity="0.3" />
-                        </svg>
-                    </a></li>
-                      <li><a href="#" class="sidebar-item flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
-                        x-tooltip.placement.right="'History Transaksi'" data-sidebar="contact"> <svg class="h-7 w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M9.85714 3H4.14286C3.51167 3 3 3.51167 3 4.14286V9.85714C3 10.4883 3.51167 11 4.14286 11H9.85714C10.4883 11 11 10.4883 11 9.85714V4.14286C11 3.51167 10.4883 3 9.85714 3Z"
-                                fill="currentColor" />
-                            <path
-                                d="M9.85714 12.8999H4.14286C3.51167 12.8999 3 13.4116 3 14.0428V19.757C3 20.3882 3.51167 20.8999 4.14286 20.8999H9.85714C10.4883 20.8999 11 20.3882 11 19.757V14.0428C11 13.4116 10.4883 12.8999 9.85714 12.8999Z"
-                                fill="currentColor" fill-opacity="0.3" />
-                            <path
-                                d="M19.757 3H14.0428C13.4116 3 12.8999 3.51167 12.8999 4.14286V9.85714C12.8999 10.4883 13.4116 11 14.0428 11H19.757C20.3882 11 20.8999 10.4883 20.8999 9.85714V4.14286C20.8999 3.51167 20.3882 3 19.757 3Z"
-                                fill="currentColor" fill-opacity="0.3" />
-                            <path
-                                d="M19.757 12.8999H14.0428C13.4116 12.8999 12.8999 13.4116 12.8999 14.0428V19.757C12.8999 20.3882 13.4116 20.8999 14.0428 20.8999H19.757C20.3882 20.8999 20.8999 20.3882 20.8999 19.757V14.0428C20.8999 13.4116 20.3882 12.8999 19.757 12.8999Z"
-                                fill="currentColor" fill-opacity="0.3" />
-                        </svg>
-                    </a></li>
-                      <li><a href="{{ url('articles') }}" class="sidebar-item flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
-                        x-tooltip.placement.right="'Article'" data-sidebar="contact">                    <svg class="h-7 w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-opacity="0.5"
-                            d="M14.2498 16C14.2498 17.5487 13.576 18.9487 12.4998 19.9025C11.5723 20.7425 10.3473 21.25 8.99976 21.25C6.10351 21.25 3.74976 18.8962 3.74976 16C3.74976 13.585 5.39476 11.5375 7.61726 10.9337C8.22101 12.4562 9.51601 13.6287 11.1173 14.0662C11.5548 14.1887 12.0185 14.25 12.4998 14.25C12.981 14.25 13.4448 14.1887 13.8823 14.0662C14.1185 14.6612 14.2498 15.3175 14.2498 16Z"
-                            fill="currentColor" />
-                        <path
-                            d="M17.75 9.00012C17.75 9.68262 17.6187 10.3389 17.3825 10.9339C16.7787 12.4564 15.4837 13.6289 13.8825 14.0664C13.445 14.1889 12.9813 14.2501 12.5 14.2501C12.0187 14.2501 11.555 14.1889 11.1175 14.0664C9.51625 13.6289 8.22125 12.4564 7.6175 10.9339C7.38125 10.3389 7.25 9.68262 7.25 9.00012C7.25 6.10387 9.60375 3.75012 12.5 3.75012C15.3962 3.75012 17.75 6.10387 17.75 9.00012Z"
-                            fill="currentColor" />
-                        <path fill-opacity="0.3"
-                            d="M21.25 16C21.25 18.8962 18.8962 21.25 16 21.25C14.6525 21.25 13.4275 20.7425 12.5 19.9025C13.5763 18.9487 14.25 17.5487 14.25 16C14.25 15.3175 14.1187 14.6612 13.8825 14.0662C15.4837 13.6287 16.7787 12.4562 17.3825 10.9337C19.605 11.5375 21.25 13.585 21.25 16Z"
-                            fill="currentColor" />
-                    </svg></a></li>
-                    </ul>
-                  </div>
-                  
 
             <!-- Main Sections Links -->
-            {{-- <div class="is-scrollbar-hidden flex grow flex-col space-y-4 overflow-y-auto pt-6">
-                <!-- Dashobards -->               
-
-                <a href="{{ url('/category') }}"
+            <div class="is-scrollbar-hidden flex grow flex-col space-y-4 overflow-y-auto pt-6">
+                <!-- Dashobards -->
+                <a href="{{ url('/category') }}" 
                     class="flex h-11 w-11 items-center justify-center rounded-lg bg-primary/10 text-primary outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:bg-navy-600 dark:text-accent-light dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90"
                     x-tooltip.placement.right="'Category'">
                     <svg class="h-7 w-7" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
@@ -119,10 +35,10 @@
                 </a>
 
                 <!-- Apps -->
-                <a href="{{ url('/product') }}"
-                    class="flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
+                <a href="{{ url('/product') }}" data-sidebar="product"
+                    class="sidebar-item flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
                     x-tooltip.placement.right="'Product'">
-                    <svg class="h-7 w-7" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                    <svg class=" h-7 w-7" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
                         <path
                             d="M248 0H208c-26.5 0-48 21.5-48 48V160c0 35.3 28.7 64 64 64H352c35.3 0 64-28.7 64-64V48c0-26.5-21.5-48-48-48H328V80c0 8.8-7.2 16-16 16H264c-8.8 0-16-7.2-16-16V0zM64 256c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H224c35.3 0 64-28.7 64-64V320c0-35.3-28.7-64-64-64H184v80c0 8.8-7.2 16-16 16H120c-8.8 0-16-7.2-16-16V256H64zM352 512H512c35.3 0 64-28.7 64-64V320c0-35.3-28.7-64-64-64H472v80c0 8.8-7.2 16-16 16H408c-8.8 0-16-7.2-16-16V256H352c-15 0-28.8 5.1-39.7 13.8c4.9 10.4 7.7 22 7.7 34.2V464c0 12.2-2.8 23.8-7.7 34.2C323.2 506.9 337 512 352 512z"
                             fill="currentColor" />
@@ -141,7 +57,7 @@
                 </a>
 
                 <a href="{{ url('transaction-list') }}"
-                    class="flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
+                    class="sidebar-item flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
                     x-tooltip.placement.right="'History Transaksi'">
                     <svg class="h-7 w-7" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                         <path
@@ -177,7 +93,7 @@
                             fill="currentColor" />
                     </svg>
                 </a>
-            </div> --}}
+            </div>
 
             <!-- Bottom Links -->
             <div class="flex flex-col items-center space-y-3 py-3">
@@ -239,15 +155,17 @@
     </div>
 </div>
 
-</body>
 
+</body>
 <script>
-// Get all the sidebar items
+    // Get all the sidebar items
 const sidebarItems = document.querySelectorAll('.sidebar-item');
 
 // Add click event listener to each sidebar item
 sidebarItems.forEach(item => {
-  item.addEventListener('click', function() {
+  item.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+
     // Remove "active" class from the previously active item
     const activeSidebarItem = document.querySelector('.sidebar-item.active');
     if (activeSidebarItem) {
@@ -257,22 +175,21 @@ sidebarItems.forEach(item => {
     // Add "active" class to the clicked sidebar item
     this.classList.add('active');
 
-    // Store the active item in local storage
+    // Store the active item in session storage
     const sidebarId = this.getAttribute('data-sidebar');
-    localStorage.setItem('activeSidebarItem', sidebarId);
+    sessionStorage.setItem('activeSidebarItem', sidebarId);
   });
+
+  // Check if there is a stored active item in session storage
+  const storedActiveSidebarItem = sessionStorage.getItem('activeSidebarItem');
+  if (storedActiveSidebarItem) {
+    // Add "active" class to the stored active item
+    if (item.getAttribute('data-sidebar') === storedActiveSidebarItem) {
+      item.classList.add('active');
+    }
+  }
 });
 
-// Check if there is an active item stored in local storage
-const storedActiveSidebarItem = localStorage.getItem('activeSidebarItem');
-if (storedActiveSidebarItem) {
-  // Add "active" class to the stored active item
-  const activeSidebarItem = document.querySelector(`.sidebar-item[data-sidebar="${storedActiveSidebarItem}"]`);
-  if (activeSidebarItem) {
-    activeSidebarItem.classList.add('active');
-  }
-}
 </script>
-
 
 </html>
