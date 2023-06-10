3bfe374257cf51de08c8bb710d8fb56d0168d095
akhipo14 <akhipo14@gmail.com>
2023-06-10 19:30:31 +0700

    cek

--- resources/views/dashboard/product/edit.blade.php
+++ resources/views/dashboard/product/edit.blade.php
@@ -23,7 +23,6 @@
                             <div class="mt-2">
                                 <label class="block text-sm font-medium leading-6 text-gray-900">Price</label>
                                 <input type="number" name="price"
-                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('price') is_invalid @enderror"
                                     value="{{ $products->price }}">
                                 <div class="text-sm text-red-600">
                                     @error('price')
