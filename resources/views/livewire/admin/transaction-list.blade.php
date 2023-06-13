f4f695f1d17601b2d87659b937663795b8e97dee
xellyx <79205130+xellyx@users.noreply.github.com>
2023-06-13 07:37:40 +0700

    revisi styling trx list

--- resources/views/livewire/admin/transaction-list.blade.php
+++ resources/views/livewire/admin/transaction-list.blade.php
@@ -112,24 +112,44 @@
                         </td>
                         <td class="px-6 py-4 text-right">
                             <div style="display: flex; justify-content: center;">
-                            @if (auth()->check() && auth()->user()->is_admin)
-                                <select wire:model="selectedStatus.{{ $item->id }}"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                    {{-- <option selected>
+                                @if (auth()->check() && auth()->user()->is_admin)
+                                    <select wire:model="selectedStatus.{{ $item->id }}"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        {{-- <option selected>
                                     Change Status
                                     </option> --}}
-                                    @foreach ($status as $stat)
-                                        <option value="{{ $stat }}">{{ $stat }}</option>
-                                    @endforeach
-                                </select>
-                                <div style="text-align: center; padding-top: 5px;">
-                                <button style="margin-left: 13px; background-color: #ffffff; color: white; padding: 8px 18px; border: 1px solid #ccc; border-radius: 10px; cursor: pointer; transition: background-color 0.3s ease; "
-        onmouseover="this.style.backgroundColor='#ADD8E6'"
-        onmouseout="this.style.backgroundColor='#ADD8E6'" wire:click="updateStatus({{ $item->id }})"><svg class="svg-icon" style="width: 24px; height: 24px;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M165.840081 127.921872h608.774771l121.254934 121.254934v608.774771c0 20.834181-17.084028 37.91821-37.918209 37.918209H165.840081c-20.834181 0-37.91821-17.084028-37.918209-37.918209V165.840081c0-20.834181 17.084028-37.91821 37.918209-37.918209z" fill="#01579B" /><path d="M284.803255 127.921872h454.39349V318.76297c0 25.626043-20.834181 46.668566-46.668566 46.668566H331.471821c-25.626043 0-46.668566-20.834181-46.668566-46.668566V127.921872zM743.155239 896.078128H280.844761V654.818311c0-27.917803 22.709257-50.62706 50.62706-50.62706h361.2647c27.917803 0 50.62706 22.709257 50.62706 50.62706l-0.208342 241.259817z" fill="#0277BD" /><path d="M299.803866 127.921872h424.392268V318.76297c0 17.29237-14.167243 31.667955-31.667955 31.667956H331.471821c-17.29237 0-31.667955-14.167243-31.667955-31.667956V127.921872zM299.803866 896.078128h424.392268V654.818311c0-17.29237-14.167243-31.667955-31.667955-31.667955H331.471821c-17.29237 0-31.667955 14.167243-31.667955 31.667955v241.259817z" fill="#EEEEEE" /><path d="M572.731638 127.921872h93.128789v181.882401h-93.128789z" fill="#424242" /><path d="M360.431333 724.612818h303.137334v19.167446H360.431333zM360.431333 808.991251h303.137334v19.167447H360.431333z" fill="#757575" /></svg></button>
+                                        @foreach ($status as $stat)
+                                            <option value="{{ $stat }}">{{ $stat }}</option>
+                                        @endforeach
+                                    </select>
+                                    <div style="text-align: center; padding-top: 5px;">
+                                        <button
+                                            style="margin-left: 13px; background-color: #ffffff; color: white; padding: 8px 18px; border: 1px solid #ccc; border-radius: 10px; cursor: pointer; transition: background-color 0.3s ease; "
+                                            onmouseover="this.style.backgroundColor='#ADD8E6'"
+                                            onmouseout="this.style.backgroundColor='#ADD8E6'"
+                                            wire:click="updateStatus({{ $item->id }})"><svg class="svg-icon"
+                                                style="width: 24px; height: 24px;vertical-align: middle;fill: currentColor;overflow: hidden;"
+                                                viewBox="0 0 1024 1024" version="1.1"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M165.840081 127.921872h608.774771l121.254934 121.254934v608.774771c0 20.834181-17.084028 37.91821-37.918209 37.918209H165.840081c-20.834181 0-37.91821-17.084028-37.918209-37.918209V165.840081c0-20.834181 17.084028-37.91821 37.918209-37.918209z"
+                                                    fill="#01579B" />
+                                                <path
+                                                    d="M284.803255 127.921872h454.39349V318.76297c0 25.626043-20.834181 46.668566-46.668566 46.668566H331.471821c-25.626043 0-46.668566-20.834181-46.668566-46.668566V127.921872zM743.155239 896.078128H280.844761V654.818311c0-27.917803 22.709257-50.62706 50.62706-50.62706h361.2647c27.917803 0 50.62706 22.709257 50.62706 50.62706l-0.208342 241.259817z"
+                                                    fill="#0277BD" />
+                                                <path
+                                                    d="M299.803866 127.921872h424.392268V318.76297c0 17.29237-14.167243 31.667955-31.667955 31.667956H331.471821c-17.29237 0-31.667955-14.167243-31.667955-31.667956V127.921872zM299.803866 896.078128h424.392268V654.818311c0-17.29237-14.167243-31.667955-31.667955-31.667955H331.471821c-17.29237 0-31.667955 14.167243-31.667955 31.667955v241.259817z"
+                                                    fill="#EEEEEE" />
+                                                <path d="M572.731638 127.921872h93.128789v181.882401h-93.128789z"
+                                                    fill="#424242" />
+                                                <path
+                                                    d="M360.431333 724.612818h303.137334v19.167446H360.431333zM360.431333 808.991251h303.137334v19.167447H360.431333z"
+                                                    fill="#757575" />
+                                            </svg></button>
+                                    </div>
+                                @else
+                                @endif
                             </div>
-                            @else
-                            @endif
-                        </div>
                             {{-- <a href="#"
                                 class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> --}}
                         </td>
